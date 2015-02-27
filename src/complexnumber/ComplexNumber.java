2603e8db73e28cc5262a3c34e898aa0646b45655
Francesco Trotta <frtrotta@users.noreply.github.com>
2015-02-27 21:18:09 +0100

    Completato, omettendo cotruttore per QuadraticEquationSolver, a causa del valore di a.

--- src/complexnumber/ComplexNumber.java
+++ src/complexnumber/ComplexNumber.java
@@ -4,6 +4,16 @@ public class ComplexNumber {
     private float re;
     private float im;
     
+    public ComplexNumber(float re, float im) {
+        this.re = re;
+        this.im = im;
+    }
+    
+    public ComplexNumber() {
+        this.re = 0;
+        this.im = 0;
+    }
+    
     public String formatComplexNumber() {
         String r = this.getRe() + "+(" + this.getIm() + ")i";
         return r;
